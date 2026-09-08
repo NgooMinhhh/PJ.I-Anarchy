3ec400d703d3eca93ff968fbca662345379e5c0c
NgooMinhhh <ngoquangminhtmg@gmail.com>
2026-09-08 18:07:09 +0700

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,21 +1,37 @@
 package com.example.addon;
 
-import com.example.addon.modules.CrystalAuraPlus;
+import com.example.addon.modules.*;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.item.Items;
 
 public class AddonTemplate extends MeteorAddon {
-    // Tạo tab "Anarchy" riêng trên ClickGUI
-    public static final Category ANARCHY_CATEGORY = new Category("Anarchy", Items.NETHERITE_SWORD.getDefaultStack());
+    public static final Category ANARCHY_CATEGORY = new Category("Anarchy 2y2c", Items.NETHERITE_SWORD.getDefaultStack());
+    public static final HudGroup HUD_GROUP = new HudGroup("Anarchy HUD");
 
     @Override
     public void onInitialize() {
-        // Đăng ký module CrystalAura+
-        Modules.get().add(new CrystalAuraPlus());
+        // Đăng ký đúng tên các Class Module theo cây thư mục dự án
+        Modules.get().add(new AnchorAura());
+        Modules.get().add(new AntiHoleFill());
+        Modules.get().add(new AutoSprint());
+        Modules.get().add(new CrystalAura());
+        Modules.get().add(new CrystalPiston());
+        Modules.get().add(new NoSlow());
+        Modules.get().add(new Offhand());
+        Modules.get().add(new SchematicPrinter());
+        Modules.get().add(new Surround());
+        Modules.get().add(new Velocity());
+
+        // Đăng ký HUD Element hiển thị danh sách Module
+        Hud.get().register(GrimArrayListHud.INFO);
     }
 
     @Override
     public String getPackage() {
         return "com.example.addon";
+    }
+}
