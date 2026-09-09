6cde694e45061299ee35ce6292825ec7e6d61484
NgooMinhhh <ngoquangminhtmg@gmail.com>
2026-09-09 12:57:00 +0700

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -2,32 +2,33 @@ package com.example.addon;
 
 import com.example.addon.modules.*;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
-import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import net.minecraft.item.Items;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AddonTemplate extends MeteorAddon {
-    public static final Category ANARCHY_CATEGORY = new Category("Anarchy 2y2c", Items.NETHERITE_SWORD.getDefaultStack());
-    public static final HudGroup HUD_GROUP = new HudGroup("Anarchy HUD");
+    public static final Logger LOG = LoggerFactory.getLogger("Project I");
 
     @Override
     public void onInitialize() {
-        // Đăng ký đúng tên các Class Module theo cây thư mục dự án
-        Modules.get().add(new AnchorAura());
-        Modules.get().add(new AntiHoleFill());
-        Modules.get().add(new AutoSprint());
-        Modules.get().add(new CrystalAura());
-        Modules.get().add(new CrystalPiston());
-        Modules.get().add(new NoSlow());
-        Modules.get().add(new Offhand());
-        Modules.get().add(new SchematicPrinter());
-        Modules.get().add(new Surround());
-        Modules.get().add(new Velocity());
+        LOG.info("Initializing Project I...");
 
-        // Đăng ký HUD Element hiển thị danh sách Module
-        Hud.get().register(GrimArrayListHud.INFO);
+        Modules.get().add(new AnchorAuraPJI());
+        Modules.get().add(new AntiHoleFillPJI());
+        Modules.get().add(new AutoSprintPJI());
+        Modules.get().add(new CrystalAuraPJI());
+        Modules.get().add(new CrystalPistonPJI());
+        Modules.get().add(new NoSlowPJI());
+        Modules.get().add(new OffhandPJI());
+        Modules.get().add(new SchematicPrinterPJI());
+        Modules.get().add(new SurroundPJI());
+        Modules.get().add(new VelocityPJI());
+
+        LOG.info("Project I initialized.");
+    }
+
+    @Override
+    public void onRegisterCategories() {
     }
 
     @Override
